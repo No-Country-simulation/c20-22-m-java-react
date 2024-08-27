733615bd759c42f8d2867bd77c855126b00f1e5d
Luz Lamas <luzlamas604@gmail.com>
2024-08-27 11:00:51 -0400

    nuevo intento

--- front/src/components/navbar/Navbar.jsx
+++ front/src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@
 import styles from './Navbar.module.css'
 
 function Navbar() {
-	return <nav className={styles.navbar}>Esto sera el navbar hola.. </nav>
+	return <nav className={styles.navbar}>Esto sera el navbar hola.... </nav>
 }
 
 export default Navbar
