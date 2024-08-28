adf56b1469869ff3ea6dcb507b01d68ba92b322b
Gabriel Gotlib <gagotlib@gmail.com>
2024-08-28 16:43:48 +0200

    adaptacion del home

--- front/src/views/home/Home.jsx
+++ front/src/views/home/Home.jsx
@@ -3,11 +3,18 @@
 import styles from './Home.module.css'
 
 function Home() {
+
+	// const posteos = axios.get(rutaasdasdasd/posts)
+
+
 	return (
 		<main className={styles.container}>
-			<h1 className={styles.title}>Título de la app</h1>
-			<p className={styles.subtitle}>Perdiste tu mascota? Encontraste una mascota perdida? </p>
-			<p className={styles.subtitle}>Aquí las mascotas y sus humanos se reencuentran</p>
+			<h1 className={styles.title}>Bienvenido a Mascotas Conectadas!</h1>
+			<p className={styles.subtitle}>¡Tu comunidad para reunir mascotas perdidas y encontradas! </p>
+			<p className={styles.text}>
+				Aquí entendemos lo importante que es para ti encontrar a tu compañero peludo. Por eso, hemos creado una plataforma simple y efectiva para ayudarte a reunirte con tu mascota o para que puedas
+				ayudar a otros a encontrar la suya
+			</p>
 
 			<div className={styles.buttonsContainer}>
 				<button type='button' className={styles.buttonPerdi} onClick={() => console.log('click')}>
