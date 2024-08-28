df1feb0f75489bc26e34cd0819f864770ce7bf15
Gabriel Gotlib <gagotlib@gmail.com>
2024-08-28 14:37:49 +0200

    algunas mejoras de diseño de navbar

--- front/src/components/navbar/Navbar.jsx
+++ front/src/components/navbar/Navbar.jsx
@@ -1,15 +1,24 @@
-// import React from 'react'
-import styles from './Navbar.module.css'
+import  { useState } from 'react'
 import { Link } from 'react-router-dom'
+import styles from './Navbar.module.css'
 
 function Navbar() {
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+	const toggleMobileMenu = () => {
+		setIsMobileMenuOpen(!isMobileMenuOpen)
+	}
+
 	return (
-		<nav className={styles.navBar}>
-			<ul className={styles.navBarList}>
+		<nav className={styles.navbar}>
+			<div className={styles.navbarTitle}>Mi App</div>
+			<button className={styles.hamburger} onClick={toggleMobileMenu}>
+				☰
+			</button>
+			<ul className={`${styles.navbarList} ${isMobileMenuOpen ? styles.navbarListOpen : ''}`}>
 				<li>
 					<Link to='/'>Home</Link>
 				</li>
-			
 				<li>
 					<Link to='/login'>Iniciar sesión</Link>
 				</li>
