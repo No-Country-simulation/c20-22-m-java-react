522cee53d03846666f2bca547ed3cc92ea054c50
DahierQuintero <dilandahier@gmail.com>
2024-08-29 17:29:50 -0500

    Se inicia el proyecto

---
+++ back/petRescue/src/test/java/com/noCountry/petRescue/PetRescueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noCountry.petRescue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetRescueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
