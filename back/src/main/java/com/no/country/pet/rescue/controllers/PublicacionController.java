8ca13e9b5e328e707bf8218e36f0769a7b3012a3
Marianella <machicadomarianella@gmail.com>
2024-09-05 11:31:48 -0300

    se agrega WebConfig en util para configuración de cors

--- back/src/main/java/com/no/country/pet/rescue/controllers/PublicacionController.java
+++ back/src/main/java/com/no/country/pet/rescue/controllers/PublicacionController.java
@@ -5,10 +5,7 @@ import com.no.country.pet.rescue.services.IPublicacionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/publications")
@@ -16,7 +13,8 @@ public class PublicacionController {
     
     @Autowired
     IPublicacionService publicacionService;
-   
+
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/save")
     public ResponseEntity<PublicacionDTO> save(@RequestBody PublicacionDTO publicacion){
         return new ResponseEntity<>(publicacionService.save(publicacion), HttpStatus.OK);
